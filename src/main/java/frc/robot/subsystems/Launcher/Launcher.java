009119c95dd81e960dc61bb3d0e00aa0673c8f45
Ben Reinert <146287616+Soccertoad@users.noreply.github.com>
2025-12-19 12:16:00 -0500

    Setting up turret subsystem file

--- src/main/java/frc/robot/subsystems/Launcher/Launcher.java
+++ src/main/java/frc/robot/subsystems/Launcher/Launcher.java
@@ -5,15 +5,31 @@
 package frc.robot.subsystems.Launcher;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.Launcher.Shooter.ShooterIO;
+import frc.robot.subsystems.Launcher.Shooter.ShooterIO.ShooterIOInputs;
+import frc.robot.subsystems.Launcher.Turret.TurretIO;
+import frc.robot.subsystems.Launcher.Turret.TurretIO.TurretIOInputs;
 
 public class Launcher extends SubsystemBase {
-    /** Creates a new Turret. */
-    public Launcher() {
 
+    /*   */
+    private final TurretIO turretIO;
+    private final TurretIOInputs turretInputs = new TurretIOInputs();
+    private final ShooterIO shooterIO;
+    private final ShooterIOInputs shooterInputs = new ShooterIOInputs();
+    
+
+    /** Creates a new Turret. */
+    public Launcher(TurretIO m_TurretIO, ShooterIO m_ShooterIO) {
+        this.turretIO = m_TurretIO;
+        this.shooterIO = m_ShooterIO;
     }
 
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
+        turretIO.update(turretInputs);
+        shooterIO.update(shooterInputs);
+
     }
 }
