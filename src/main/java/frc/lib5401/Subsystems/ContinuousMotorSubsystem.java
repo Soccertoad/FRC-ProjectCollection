92cd71085e82a3716b2a49616f784a24dc731857
Ben Reinert <soccertoad22@gmail.com>
2025-12-30 16:25:00 -0500

    Base subsystems to simplify work

---
+++ src/main/java/frc/lib5401/Subsystems/ContinuousMotorSubsystem.java
@@ -0,0 +1,18 @@
+//Subsystems with unlimited range of motion, flywheels rollers, intakes
+
+package frc.lib5401.Subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ContinuousMotorSubsystem<T extends MotorIO, U extends MotorIOInputs> extends SubsystemBase{
+
+    public ContinuousMotorSubsystem(){
+
+    }
+
+    @Override
+    public void periodic(){
+        
+    }
+    
+}
---
+++ src/main/java/frc/lib5401/Subsystems/ServoMotorSubsystem.java
@@ -0,0 +1,16 @@
+// For subsystems with limited range of motion or fixed position setpoints
+
+package frc.lib5401.Subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ServoMotorSubsystem<T extends MotorIO, U extends MotorIOInputs> extends SubsystemBase {
+
+    /** Creates a new ServoMotorSubsystem. */
+    public ServoMotorSubsystem() {}
+
+    @Override
+    public void periodic() {
+    // This method will be called once per scheduler run
+    }
+}
